a8477661c6ab977b9b520f5f5646dd9036faa1aa
mahirkursun <mahirkrsn@gmail.com>
2022-08-28 00:08:49 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 //import User from "./components/User"
 import './App.css';
 import Navbar from './components/Navbar';
-import User from './components/User';
+import Users from './components/Users';
+import React, { Component } from 'react'
 
-function App() {
-
-  return (
-    <div className="container">      
-        <Navbar title ="User App"/>
-        <hr/>
-        <User 
-        name="Mustafa Murat Coşkun"
-        salary ="5000"
-        department="Bilişim"
-        />
-        <User 
-        name="Oğuz Artıran"
-        salary ="6000"
-        department="Bilişim"
-        />
-    </div>
-  );
+class App extends Component {
+  state = {
+    users : [
+      {
+        id : 1,
+        name : "Mustafa Murat Coşkun",
+        salary : "5000",
+        department : "Bilişim"
+      },
+      {
+        id : 2,
+        name : "Kemalettin Taslak",
+        salary : "4000",
+        department : "Pazarlama"
+      },
+      {
+        id : 3,
+        name : "Ahmet Tarak",
+        salary : "6000",
+        department : "Üretim"
+      }
+      
+    ]
+  }
+  render() {
+    return (
+      <div className="container">      
+          <Navbar title ="User App"/>
+          <hr/>
+          <Users users ={this.state.users}/>
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/components/User.js
+++ src/components/User.js
@@ -2,38 +2,53 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class User extends Component {
+  state={
+    isVisible : false
+  }
   static defaultProps ={
     name:"Bilgi Yok",
     salary:"Bilgi Yok",
     department:"Bilgi Yok"
   }
-constructor(props){
-  super(props);
+ 
+  onClickEvent=(e)=>{
+    this.setState({
+      isVisible : !this.state.isVisible
+    })
 
-  this.state={
-    test : "Test"
   }
-}
+// constructor(props){
+//   super(props);
+
+//   this.state={
+//     isVisible : true
+//   }
+// }
 
   render() {
 
     //Destructing
     const {name,department,salary}=this.props;
+    const {isVisible} = this.state;
     return (
       <div className='col-md-8 mb-4'>
         <div className='card'>
           <div className='card-header d-flex justify-content-between'>
-            <h4 className='d-inline'>{name}</h4>
+            <h4 className='d-inline' onClick={this.onClickEvent}>{name}</h4>
 
             <i className='far fa-trash-alt' style={{cursor:"pointer"}}></i>
 
           </div>
-          <div className='card-body'>
+          {
+            isVisible ? 
+            <div className='card-body'>
 
-            <p className='card-text'>Maaş : {salary}</p>
-            <p className='card-text'>Department : {department}</p>
-            <p>{this.state.test}</p>
-          </div>
+              <p className='card-text'>Maaş : {salary}</p>
+              <p className='card-text'>Department : {department}</p>
+           
+            </div> : null
+          }
+          
         </div>
       </div>
     )
