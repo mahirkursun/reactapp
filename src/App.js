89392b1f852542cf58eb5360de511b6307cf9776
mahirkursun <mahirkrsn@gmail.com>
2022-09-07 23:57:58 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 //import User from "./components/User"
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './layout/Navbar';
 import Users from './components/Users';
 import React, { Component } from 'react';
 import AddUser from './components/AddUser';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Contribute from './pages/Contribute';
 
 // const Home = () => {
 //   return (
@@ -30,12 +31,16 @@ class App extends Component {
         <div className="container">     
           <Navbar title ="User App"/>
           <hr/>
-          
-            <Route exact path = "/"><Users /></Route>
-            <Route exact path = "/add"><AddUser /></Route>
+            <Switch>
+              <Route exact path = "/" component={Users}/>
+              <Route exact path = "/add" component={AddUser}/>  
+              <Route exact path = "/github" component={Contribute}/>
+              <Route component = {NotFound}/>
+            </Switch>
 
-        
-            
+                  
+
+
         </div>
       </Router>
     );
---
+++ src/layout/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import propTypes from "prop-types";
+import {Link} from 'react-router-dom';
+
+ function  Navbar(props) {
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/add">Add User</Link>
+        </li>
+        <li>
+          <Link to="/github">Project Files</Link>
+        </li>
+        
+      </ul>
+    </div>
+  )
+}
+Navbar.propTypes ={
+  title : propTypes.string.isRequired
+}
+Navbar.defaultProps ={
+  title :"Default App"
+}
+export default Navbar;
